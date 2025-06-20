39ebf58a7b7cb4a3f662fd4f2757597c60d5626b
DanielJohn17 <danielyohannes38@gmail.com>
2025-06-20 23:44:48 +0300

    postgresql database connection

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,13 @@
+
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
